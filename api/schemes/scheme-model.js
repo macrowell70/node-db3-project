39891eadd67443253900e2c049cd9cd2ab1be5df
unknown <macrowell@yahoo.com>
2022-06-15 20:42:10 -0400

    set up find by id function

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -25,6 +25,12 @@ function find() { // EXERCISE A
 }
 
 function findById(scheme_id) { // EXERCISE B
+  return db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('sc.scheme_name', 'st.*')
+    .where({ 'sc.scheme_id': scheme_id })
+    // .orderBy('st.step_number')
+
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
